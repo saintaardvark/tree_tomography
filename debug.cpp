085694e98f16d5f57c6745a59a67e336efdf2f04
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2023-08-06 14:41:05 -0700

    Refactor timing counting & display
    - Hammer ISR should set start_time
    - ISR for MPU6050 should set impact_time
    - Elapsed time not yet set, but should be simple when the time comes.
    
    However, not working yet:  for some reason, start time does not seem
    to be set -- or at least, not displayed on screen or serial printing,
    even when printed in main loop.
    
    Even when I just touch the pad gently -- avoiding the case where the
    hammer interrupt and the mpu interrupt are competing for CPU time -- I
    don't see the start time changing.
    
    I *do* see state changing to 2 when the hammer makes contact.  And the

--- debug.cpp
+++ debug.cpp
@@ -2,7 +2,8 @@
 
 #include "constants.h"
 
-void maybe_debug(sensors_event_t *a, int state, int buttonState, int64_t last_time) {
+
+void maybe_debug(sensors_event_t *a, int state, int buttonState, int64_t start_time, int64_t impact_time, int64_t elapsed_time) {
   if (DEBUG > 0) {
     Serial.print("Acceleration_X:");
     Serial.print(a->acceleration.x);
@@ -14,8 +15,12 @@ void maybe_debug(sensors_event_t *a, int state, int buttonState, int64_t last_ti
     Serial.print(buttonState);
     Serial.print(",State:");
     Serial.print(state);
-    Serial.print(",LastTime:");
-    Serial.print(last_time);
+    Serial.print(",StartTime:");
+    Serial.print(start_time);
+    Serial.print(",ImpactTime:");
+    Serial.print(impact_time);
+    Serial.print(",ElapsedTime:");
+    Serial.print(elapsed_time);
     Serial.print(",Now:");
     Serial.println(esp_timer_get_time());
   }
--- debug.h
+++ debug.h
@@ -1,1 +1,1 @@
-void maybe_debug(sensors_event_t *a, int state, int buttonState, int64_t last_time);
+void maybe_debug(sensors_event_t *a, int state, int buttonState, int64_t start_time, int64_t impact_time, int64_t elapsed_time);
