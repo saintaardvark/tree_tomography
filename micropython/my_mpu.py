49947f02d0982c413c75bb2ba2a7f4f5b58ad345
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2023-08-23 07:23:37 -0700

    Break out my_mpu.py

---
+++ micropython/my_mpu.py
@@ -0,0 +1,30 @@
+from mpu6050 import MPU6050
+
+class MyMpu(MPU6050):
+    def start(self):
+        """
+        My own version of the Adafruit/eluke.nl code
+        """
+        # mpu->setMotionDetectionThreshold(1);
+        self.__writeByte(0x1F, 0x01)
+        # mpu->setMotionDetectionDuration(1);
+        self.__writeByte(0x20, 0x01)
+        # mpu->setInterruptPinLatch(true);	// Keep it latched.  Will turn off when reinitialized.
+        # Want to set 5th (latch until clear).
+        # Could *also* set 4th bit (clear by reading 0x3a / d58), but will leave that for now.
+        # WAIT: Maybe this isn't correct?
+        self.__writeByte(0x37, 0x20)
+        # mpu->setInterruptPinPolarity(true);
+        # This is config'd by setting 0x37, 7th bit to 0.  Done above.
+        # mpu->setMotionInterrupt(true);
+        # IntEnable is 0x38.  Need to set 6th bit.
+        self.__writeByte(0x38, 0x40)
+        #
+        # And finally, clear interrupts to get ready:
+        self.reset_interrupt()
+
+    def reset_interrupt(self):
+        """
+        This lets the next interrupt happen if interrupts are latched.
+        """
+        self.__readByte(0x3A)
