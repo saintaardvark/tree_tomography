fd41096c74caa44f2eb924d3fa0f8ace54edd2cb
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2023-08-24 07:49:56 -0700

    Add some more notes to counter.py

--- micropython/counter.py
+++ micropython/counter.py
@@ -6,7 +6,9 @@ def counter():
     # Set scratch register y to value 3
     set(y, 3)
     # wait for 1 (high) on pin 2.  This is the counter SM; its base
-    # pin is 14, so we're waiting on pin 16 to show high.
+    # pin is 14, so we're waiting on pin 16 to show high.  This
+    # happens when either one of the triggers detects a change on the
+    # pin they're watching; they do that for two cycles.
     wait(1, pin, 2)
     # Wrap up here
     wrap_target()
@@ -17,7 +19,8 @@ def counter():
     # the initialization of sm4 (sideset_base).
     set(x, 0).side(1)
     # Wait for 0 (low) on pin 2.  Again, the base pin is 14, so we're
-    # waiting on pin 16 to show low.
+    # waiting on pin 16 to show low.  The triggers will lower pin 16
+    # after two cycles.
     wait(0, pin, 2)
     # Shift 2 bits from the pins we're watching to the Input Shift
     # Register.  This will be read later on by the interrupt handler.
@@ -26,7 +29,8 @@ def counter():
     # ISR.
     in_(pins, 2)
     # Push the contents of the ISR into the RX fifo as one 32-bit
-    # word.  Set the ISR to all zeroes.
+    # word.  Set the ISR to all zeroes.  The RX fifo now contains the
+    # state of those two pins.
     push()
     # Jump to "counter start" if x is non-zero.  Irregardlessfully,
     # decrement x.
@@ -35,7 +39,10 @@ def counter():
     label("counter_start")
     # Jmp to label output if pin is 1 (high).  The pin we check is set
     # by jmp_pin (EXECCTRL_JMP_PIN in assembly).  For SM4, that's pin
-    # 16.
+    # 16.  Again: the triggers will raise pin 16 if there's been a
+    # change in the pin they're watching.  If they saw a HIGH up
+    # above, this time it's because they've seen the LOW coming
+    # after..
     jmp(pin, "output")
     # Jump to "counter start" if x is non-zero.  Irregardlessfully,
     # decrement x.
@@ -46,7 +53,8 @@ def counter():
     # invert those bits first.  As a side effect, set side pin 25 to 0.
     mov(isr, invert(x)).side(0)
     # Push the contents of the ISR into the RX fifo as one 32-bit
-    # word.  Set the ISR to all zeroes.
+    # word.  Set the ISR to all zeroes.  The RX fifo now contains the
+    # number of 2-cycles between the first rise & the second rise.
     push()
     # Jump to "loop" if y is non-zero.  Irregardlessfully,
     # decrement y.
