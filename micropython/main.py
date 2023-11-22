e0609f4f5819e80a9ecb6be9acadc390b469ac0c
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2023-11-22 07:45:32 -0800

    Begin to break out some functions, use main()

--- micropython/main.py
+++ micropython/main.py
@@ -31,9 +31,14 @@ start = ticks_ms()
 START_SIG_SIM = False
 SLEEPYTIME = 0.5
 
-if START_SIG_SIM is True:
-    print("Starting simulator!")
-    start_sig_sim()
+
+def maybe_start_simulator(start_sim=START_SIG_SIM):
+    """
+    Maybe start signal simulator
+    """
+    if start_sig is True:
+        print("Starting simulator!")
+        start_sig_sim()
 
 
 # Source: https://github.com/dommodnet/Pi-Pico-Pinout-Ascii-Art
@@ -66,53 +71,61 @@ if START_SIG_SIM is True:
 #          | Pi-Pico                           |
 #          +-----------------------------------+
 
+def main():
+    """
+    Main entry point
+    """
+    maybe_start_simulator()
 
-p1 = Pin(15, Pin.IN, Pin.PULL_DOWN)  # Green LED   -- pin 20
-p2 = Pin(14, Pin.IN, Pin.PULL_DOWN)  # Blue LED    -- pin 19
-p3 = Pin(13, Pin.IN, Pin.PULL_DOWN)  # Red LED     -- pin 17
-p4 = Pin(12, Pin.IN, Pin.PULL_DOWN)  # Future work -- pin 16
+    p1 = Pin(15, Pin.IN, Pin.PULL_DOWN)  # Green LED   -- pin 20
+    p2 = Pin(14, Pin.IN, Pin.PULL_DOWN)  # Blue LED    -- pin 19
+    p3 = Pin(13, Pin.IN, Pin.PULL_DOWN)  # Red LED     -- pin 17
+    p4 = Pin(12, Pin.IN, Pin.PULL_DOWN)  # Future work -- pin 16
 
-switch = Pin(18, Pin.IN, Pin.PULL_DOWN)  # pin 24
-led_1 = Pin(16, Pin.OUT)
-led_2 = Pin(17, Pin.OUT)
+    switch = Pin(18, Pin.IN, Pin.PULL_DOWN)  # pin 24
+    led_1 = Pin(16, Pin.OUT)
+    led_2 = Pin(17, Pin.OUT)
 
-pulsein_12 = pulsedelay(p1, p2)  # Time of flight between blue & green
-pulsein_13 = pulsedelay(p4, p3)  # Time of flight between blue & red
+    pulsein_12 = pulsedelay(p1, p2)  # Time of flight between blue & green
+    pulsein_13 = pulsedelay(p4, p3)  # Time of flight between blue & red
 
-# which_sm = 123  # Start by watching 1->2 *and* 1->3
-which_sm = 12  # Start by watching 1->2 only
-led_1.on()
-led_2.off()
+    # which_sm = 123  # Start by watching 1->2 *and* 1->3
+    which_sm = 12  # Start by watching 1->2 only
+    led_1.on()
+    led_2.off()
 
 
-print("Everything looks good!")
-print("Now entering state of cat-like readiness 😼...")
+    print("Everything looks good!")
+    print("Now entering state of cat-like readiness 😼...")
 
-i = 0
-while True:
-    if which_sm == 12:
-        print("1->2: ", pulsein_12.get(), " microseconds")
-    elif which_sm == 13:
-        print("1->3: ", pulsein_13.get(), " microseconds")
-    else:
-        print("1->2: ", pulsein_12.get(), " microseconds")
-        print("1->3: ", pulsein_13.get(), " microseconds")
-
-    sleep(SLEEPYTIME)
-    print("=-=-=-=-=-=-=-=-=-")
-    if switch.value() == 1:
-        print("Switching!")
+    i = 0
+    while True:
         if which_sm == 12:
-            which_sm = 13
-            led_1.off()
-            led_2.on()
+            print("1->2: ", pulsein_12.get(), " microseconds")
         elif which_sm == 13:
-            which_sm = 123
-            led_1.on()
-            led_2.on()
-        elif which_sm == 123:
-            which_sm = 12
-            led_1.on()
-            led_2.off()
+            print("1->3: ", pulsein_13.get(), " microseconds")
+        else:
+            print("1->2: ", pulsein_12.get(), " microseconds")
+            print("1->3: ", pulsein_13.get(), " microseconds")
 
-        print(f"Now looking at {which_sm}")
+        sleep(SLEEPYTIME)
+        print("=-=-=-=-=-=-=-=-=-")
+        if switch.value() == 1:
+            print("Switching!")
+            if which_sm == 12:
+                which_sm = 13
+                led_1.off()
+                led_2.on()
+            elif which_sm == 13:
+                which_sm = 123
+                led_1.on()
+                led_2.on()
+            elif which_sm == 123:
+                which_sm = 12
+                led_1.on()
+                led_2.off()
+
+            print(f"Now looking at {which_sm}")
+
+if __name__ == '__main__':
+    main()
