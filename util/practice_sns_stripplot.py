2b93efe33a873c39b9ec57f0368074d2ff121b7b
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2023-12-28 16:03:31 -0500

    Print out both histplot & stripplot at once

--- util/practice_sns_stripplot.py
+++ util/practice_sns_stripplot.py
@@ -29,27 +29,29 @@ class DynamicUpdate:
     def update(self, x, y):
         self.xdata.append(x)
         self.ydata.append(y)
-        self.update_graph()
 
     def on_launch(self):
         # Set up plot
-        self.figure, self.ax = plt.subplots()
+        self.figure, self.ax = plt.subplots(2, 1)
         # (self.lines,) = self.ax.plot([], [], "o")
         # Autoscale on unknown axis and known lims on the other
-        self.ax.set_autoscaley_on(True)
-        self.ax.set_xlim(self.min_x, self.max_x)
-        # Other stuff
-        self.ax.grid()
+        print(len(self.ax))
+        for ax in self.ax:
+            ax.set_autoscaley_on(True)
+            ax.set_xlim(self.min_x, self.max_x)
+            ax.grid()
 
     def update_graph(self):
         # Update data (with the new _and_ the old points)
         # self.lines.set_xdata(self.xdata)
         # self.lines.set_ydata(self.ydata)
         plt.cla()
-        sns.stripplot(data=self.ydata, ax=self.ax)
+        sns.stripplot(data=self.ydata, ax=self.ax[0])
+        sns.histplot(data=self.ydata, ax=self.ax[1])
         # Need both of these in order to rescale
-        self.ax.relim()
-        self.ax.autoscale_view()
+        for ax in self.ax:
+            ax.relim()
+            ax.autoscale_view()
         # We need to draw *and* flush
         self.figure.canvas.draw()
         self.figure.canvas.flush_events()
@@ -70,6 +72,8 @@ def main():
             y = float(line)
             d.update(x, y)
             x += 1
+            if x % 10 == 0:
+                d.update_graph()
 
 
 if __name__ == "__main__":
