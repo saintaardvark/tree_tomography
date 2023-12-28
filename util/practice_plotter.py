dd07d14ef7c90356dff30fbe390c8beccc7ca8bb
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2023-12-28 07:55:26 -0500

    Use seaborn stripplot
    Not perfect yet -- overlays graph instead of flushing & drawing new one

--- util/practice_plotter.py
+++ util/practice_plotter.py
@@ -9,6 +9,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
+import seaborn as sns
 
 
 class DynamicUpdate:
@@ -18,9 +19,9 @@ class DynamicUpdate:
     max_x = 10
 
     def __init__(self):
-        self.on_launch()
         self.xdata = []
         self.ydata = []
+        self.on_launch()
 
     def update(self, x, y):
         self.xdata.append(x)
@@ -30,7 +31,7 @@ class DynamicUpdate:
     def on_launch(self):
         # Set up plot
         self.figure, self.ax = plt.subplots()
-        (self.lines,) = self.ax.plot([], [], "o")
+        # (self.lines,) = self.ax.plot([], [], "o")
         # Autoscale on unknown axis and known lims on the other
         self.ax.set_autoscaley_on(True)
         self.ax.set_xlim(self.min_x, self.max_x)
@@ -39,15 +40,15 @@ class DynamicUpdate:
 
     def update_graph(self):
         # Update data (with the new _and_ the old points)
-        self.lines.set_xdata(self.xdata)
-        self.lines.set_ydata(self.ydata)
+        # self.lines.set_xdata(self.xdata)
+        # self.lines.set_ydata(self.ydata)
+        sns.stripplot(data=self.ydata, ax=self.ax)
         # Need both of these in order to rescale
         self.ax.relim()
         self.ax.autoscale_view()
         # We need to draw *and* flush
         self.figure.canvas.draw()
         self.figure.canvas.flush_events()
-
 
 
 def main():
