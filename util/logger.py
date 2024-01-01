bc366290f1efcdff1cafcfb90412b6f07afbefb0
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2024-01-01 10:48:23 -0800

    Use seaborn plotting for logger.py

--- util/logger.py
+++ util/logger.py
@@ -11,7 +11,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
-
+import seaborn as sns
 
 DATA = []
 
@@ -35,7 +35,19 @@ def graph(data):
     Graph data in some way
     """
     print(f"Here's a graph!", data)
-    plt.plot(data)
+    # plt.plot(data)
+    # plt.show()
+    figure, ax = plt.subplots(1, 4)
+    # FIXME: axx is a poor variable name
+    for axx in ax:
+        axx.set_autoscaley_on(True)
+        # TODO: See if xlim makes sense here
+        # axx.set_xlim(min_x, max_x)
+        axx.grid()
+    sns.stripplot(data=data, ax=ax[0])
+    sns.scatterplot(data=data, ax=ax[1])
+    sns.histplot(data=data, ax=ax[3])
+    sns.boxplot(data=data, ax=ax[2])
     plt.show()
 
 
