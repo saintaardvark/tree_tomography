a27b979c27478acf905f8c3218a085866f9533fe
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2024-09-02 15:32:03 -0700

    Add some comments

--- micropython-parallel-read/firmware/paral.py
+++ micropython-parallel-read/firmware/paral.py
@@ -13,14 +13,14 @@ from rp2 import PIO, StateMachine, asm_pio
     push_thresh=32,
 )
 def clock():
-    set(x, 0)
-    label("WAIT_FOR_P1_HIGH")
+    set(x, 0)  # Set x to 0
+    label("WAIT_FOR_P1_HIGH")  # Label for waiting loop
     # Wait for pin1 to be high
-    jmp(pin, "PUSH")
-    jmp(x_dec, "WAIT_FOR_P1_HIGH")
-    label("PUSH")
-    in_(x, 32)
-    jmp(x_dec, "WAIT_FOR_P1_HIGH")
+    jmp(pin, "PUSH")  # if pin1 is high, jump to PUSH loop.
+    jmp(x_dec, "WAIT_FOR_P1_HIGH")  # Otherwise, decrement x & go to top of waiting loop
+    label("PUSH")  # Beginning of PUSH loop
+    in_(x, 32)  # Move 32 bits of x into [ISR?]
+    jmp(x_dec, "WAIT_FOR_P1_HIGH")  # dec x & go to top of waiting loop
 
 
 @asm_pio(
@@ -34,13 +34,13 @@ def paral_read():
     """
     Read in parallel
     """
-    mov(y, pins)
-    label("main_loop")
-    mov(x, pins)
-    jmp(x_not_y, "move_out")
-    jmp("main_loop")
-    label("move_out")
-    in_(x, 8).side(1)
-    push().side(0)
-    mov(y, x)
-    jmp("main_loop")
+    mov(y, pins)  # Move current state of pins into y
+    label("main_loop")  # Beginning of main loop
+    mov(x, pins)  # Move current state of pins into x
+    jmp(x_not_y, "move_out")  # If x !=y, jump to move out
+    jmp("main_loop")  # Otherwise, go back to reading pins
+    label("move_out")  # Beginning of move_out loop
+    in_(x, 8).side(1)  # Move 8 bits of x into [ISR?]; side effect: set JMP_PIN to 1
+    push().side(0)  # Push out ISR; side effect: set JMP_PIN to 0
+    mov(y, x)  # Move x to y
+    jmp("main_loop")  # Go back to main loop
